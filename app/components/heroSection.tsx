74c11ddf28836800c901a93803f1db5d3bae9e79
raphael furini <raphael.furini@hotmail.com>
2024-01-19 11:31:00 -0300

    feat: inserido dados no banco

--- app/components/heroSection.tsx
+++ app/components/heroSection.tsx
@@ -1,7 +1,11 @@
-export function HeroSection (){
-  return(
-    <div className="">
-
-    </div>
-  )
+import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient();
+let champs = [];
+async function getData() {
+  champs = await prisma.champions.findMany();
+  console.log(champs);
+}
+getData();
+export function HeroSection() {
+  return <div className="">{champs[0].driver}</div>;
 }
